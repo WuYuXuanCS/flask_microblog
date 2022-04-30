d371da0f6cf4c0bc3b10bde8cab1cc360b2855f2
WuYuXuanCS <915664196@qq.com>
2022-04-30 21:58:21 +0800

    使用Flask实现符合REST架构的API

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ import bleach
 from markdown import markdown
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app, request
+from flask import current_app, request, url_for
 from flask_login import UserMixin, AnonymousUserMixin
+from wtforms import ValidationError
+
 from . import db, login_manager
 from datetime import datetime
 
@@ -157,6 +159,20 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    # verify_auth_token() 是静态方法，因为只有解码令牌后才能知道用户是谁。
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
@@ -248,6 +264,18 @@ class User(UserMixin, db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    # 把用户转换成JSON 格式的序列化字典
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_user', id=self.id),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts_url': url_for('api.get_user_posts', id=self.id),
+            'followed_posts_url': url_for('api.get_user_followed_posts', id=self.id),
+            'post_count': self.posts.count()
+        }
+        return json_user
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -263,6 +291,29 @@ class Post(db.Model):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
                         'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
         target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+
+    # 把文章转换成JSON 格式的序列化字典
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id=self.id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.author_id),
+            'comments_url': url_for('api.get_post_comments', id=self.id),
+            'comment_count': self.comments.count()
+
+        }
+        return json_post
+
+    # JSON结构反序列化成模型
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('博客没有内容')
+        return Post(body=body)
+
 
 
 # on_changed_body() 函数注册在body 字段上，是SQLAlchemy“set”事件的监听程序
